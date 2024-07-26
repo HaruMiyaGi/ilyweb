3f5e54974434bc664ad2cf81312df77bf0d101b6
HaruMiyaGi <harumiyagi@mail.ru>
2024-07-26 21:34:30 +0100

    create simple todo ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,51 @@
+import "@aws-amplify/ui-react/styles.css";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import outputs from "@/../amplify_outputs.json";
 import { Amplify } from "aws-amplify";
-import { Authenticator } from "@aws-amplify/ui-react";
+import {
+  Authenticator,
+  Button,
+  Flex,
+  Heading,
+  Theme,
+  View,
+} from "@aws-amplify/ui-react";
+import { ThemeProvider } from "@aws-amplify/ui-react";
+import { IconLogout } from "@tabler/icons-react";
 
 Amplify.configure(outputs);
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user?.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <Authenticator>
+          {({ signOut, user }) => (
+            <main>
+              <Flex alignItems={"center"}>
+                <View flex={1}>
+                  <Heading>ilyweb</Heading>
+                </View>
+                <View>
+                  <Button size="small" gap="0.3rem" onClick={signOut}>
+                    <IconLogout />
+                    Sign Out
+                  </Button>
+                </View>
+              </Flex>
+
+              <Component {...pageProps} />
+            </main>
+          )}
+        </Authenticator>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,76 @@
+import {
+  Alert,
+  Button,
+  Card,
+  Collection,
+  Flex,
+  Loader,
+  Text,
+} from "@aws-amplify/ui-react";
 import Head from "next/head";
+import { generateClient } from "aws-amplify/data";
+import { Schema } from "../../amplify/data/resource";
+import { useQuery } from "@tanstack/react-query";
+import { useQueryClient, useMutation } from "@tanstack/react-query";
+import TodoCreateForm from "../../ui-components/TodoCreateForm";
+import { IconEdit, IconEditOff, IconTrash } from "@tabler/icons-react";
+import { useState } from "react";
+import TodoUpdateForm from "../../ui-components/TodoUpdateForm";
+
+const client = generateClient<Schema>();
 
 export default function Home() {
+  const queryClient = useQueryClient();
+
+  const { data, isLoading, isSuccess, isError } = useQuery({
+    queryKey: ["todoKey"],
+    queryFn: async () => {
+      const { data } = await client.models.Todo.list();
+      return !data ? null : data;
+    },
+  });
+
+  const deleteMutation = useMutation({
+    mutationFn: async (id: string) => {
+      const { data } = await client.models.Todo.delete({ id });
+      return data;
+    },
+    // When mutate is called:
+    onMutate: async (id) => {
+      // Cancel any outgoing refetches
+      // (so they don't overwrite our optimistic update)
+      await queryClient.cancelQueries({ queryKey: ["todoKey", id] });
+      await queryClient.cancelQueries({ queryKey: ["todoKey"] });
+
+      // Snapshot the previous value
+      const prevData = queryClient.getQueryData(["todoKey", id]);
+
+      // Optimistically update to the new value
+      if (prevData) {
+        queryClient.setQueryData(["todoKey", id], id);
+      }
+
+      // Return a context with the previous and new realEstateProperty
+      return { prevData, id };
+    },
+    // If the mutation fails, use the context we returned above
+    onError: (err, data, context) => {
+      console.error("Error deleting record:", err, data);
+      if (context?.prevData) {
+        queryClient.setQueryData(["todoKey", context.id], context.prevData);
+      }
+    },
+    // Always refetch after error or success:
+    onSettled: (data) => {
+      if (data) {
+        queryClient.invalidateQueries({ queryKey: ["todoKey", data.id] });
+        queryClient.invalidateQueries({ queryKey: ["todoKey"] });
+      }
+    },
+  });
+
+  const [editItemId, setEditItemId] = useState<string | null>();
+
   return (
     <>
       <Head>
@@ -10,7 +80,94 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <p>ilyweb</p>
+      <TodoCreateForm
+        onSuccess={() => {
+          queryClient.invalidateQueries({ queryKey: ["todoKey"] });
+        }}
+        onError={() => {
+          queryClient.invalidateQueries({ queryKey: ["todoKey"] });
+        }}
+      />
+
+      {isLoading ? (
+        <Loader variation="linear" />
+      ) : (
+        <>
+          {isError && (
+            <Alert variation="error">
+              <Text>Unexpected Error</Text>
+            </Alert>
+          )}
+
+          {isSuccess && (
+            <Collection items={data!} type="list">
+              {(item, index) => (
+                <Card key={index}>
+                  <Flex alignItems={"center"}>
+                    <Text flex={1}>{item.content}</Text>
+
+                    {editItemId === item.id ? (
+                      <Button //
+                        size="small"
+                        gap={"0.3rem"}
+                        colorTheme="info"
+                        onClick={() => setEditItemId(null)}
+                      >
+                        <IconEditOff />
+                        Cancel
+                      </Button>
+                    ) : (
+                      <Button //
+                        size="small"
+                        gap={"0.3rem"}
+                        colorTheme="info"
+                        onClick={() => setEditItemId(item.id)}
+                        disabled={
+                          editItemId === item.id || deleteMutation.isPending
+                        }
+                      >
+                        <IconEdit />
+                        Edit
+                      </Button>
+                    )}
+
+                    <Button
+                      size="small"
+                      gap={"0.3rem"}
+                      colorTheme="warning"
+                      onClick={() => deleteMutation.mutate(item.id)}
+                      disabled={
+                        editItemId === item.id || deleteMutation.isPending
+                      }
+                    >
+                      <IconTrash />
+                      Remove
+                    </Button>
+                  </Flex>
+
+                  {editItemId === item.id && (
+                    <TodoUpdateForm
+                      id={item.id}
+                      onSuccess={() => {
+                        queryClient.invalidateQueries({
+                          queryKey: ["todoKey"],
+                        });
+                        setEditItemId(null);
+                      }}
+                      onError={() => {
+                        queryClient.invalidateQueries({
+                          queryKey: ["todoKey"],
+                        });
+                        setEditItemId(null);
+                      }}
+                    />
+                  )}
+                </Card>
+              )}
+            </Collection>
+          )}
+        </>
+      )}
     </>
   );
 }
