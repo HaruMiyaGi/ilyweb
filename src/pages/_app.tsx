717c262517c9e54cb76ec86a723dd3deb2c2e298
HaruMiyaGi <harumiyagi@mail.ru>
2024-07-26 19:51:41 +0100

    add authentication

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import outputs from "@/../amplify_outputs.json";
+import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+
+Amplify.configure(outputs);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
 }
