fce93bd4bf1bd017b70f8479ffba90297d99f562
HaruMiyaGi <harumiyagi@mail.ru>
2024-07-26 18:10:19 +0100

    add nextjs framework

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ilyweb</title>
+        <meta name="description" content="ilyweb" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <p>ilyweb</p>
+    </>
+  );
+}
